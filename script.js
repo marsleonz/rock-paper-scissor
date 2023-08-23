28828d8e366073dfbf8076bd0b896cf0510824aa
Manik Shah <marsleonz@gmail.com>
2023-08-23 21:27:50 +0545

    Add ui to the game

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("this works");
 
 let getComputerChoice = () => {
     let randomNumber = Math.floor(Math.random()*3) ;
@@ -45,28 +44,51 @@ function playRound(playerSelection,computerSelection){
 function game(){
     let yourScore = 0;
     let computerScore = 0;
-    for(let i = 0 ; i <= 4 ; i++){
-        const playerSelection = prompt("Enter your choice");
-        const computerSelection = getComputerChoice();
-        console.log("You chose: ",playerSelection.toUpperCase());
-        console.log("Computer chose: ",computerSelection);
-        let result = playRound(playerSelection.toUpperCase(),computerSelection);
-        if (result=='You win'){
-            yourScore++;
-        }else if (result == 'You lose'){
-            computerScore++;
-        }
-        console.log(result);
-        console.log("Your Score:",yourScore);
-        console.log("Computer Score:",computerScore);
-    }
-    if (yourScore > computerScore){
-        console.log("You win this game")
-    }else if(yourScore < computerScore){
-        console.log("Computer wins this game")
-    }else{
-        console.log("Game is tied")
-    }
+    
+    let buttons = document.querySelectorAll('button');
+    let container = document.querySelector('.container');
+    let displayPlayerSelection = document.createElement('h1');
+    let displayComputerSelection = document.createElement('h1');
+    let displayResult = document.createElement('h1');
+    let displayPlayerScore = document.createElement('h1');
+    let displayComputerScore = document.createElement('h1');
+    let displayFinalResult = document.createElement('h1');
+    let displayGameOver= document.createElement('h1');
+    buttons.forEach((button) =>{
+        button.addEventListener('click',(event)=>{
+            if (yourScore >= 5 || computerScore >= 5) {
+                displayGameOver.textContent = 'The game is over'
+                container.appendChild(displayGameOver);
+                return; // Break the loop if the game is already over
+            }
+            const playerSelection = event.target.innerText;
+            const computerSelection = getComputerChoice();
+            displayPlayerSelection.textContent = `You chose:${playerSelection.toUpperCase()}`;
+            container.appendChild(displayPlayerSelection);
+            displayComputerSelection.textContent = `Computer chose:${computerSelection}`;
+            container.appendChild(displayComputerSelection);
+            let result = playRound(playerSelection.toUpperCase(),computerSelection);
+            if (result=='You win'){
+                yourScore++;
+            }else if (result == 'You lose'){
+                computerScore++;
+            }
+            displayResult.textContent = `${result}`;
+            displayPlayerScore.textContent = `Your Score: ${yourScore}`;
+            displayComputerScore.textContent = `Computer Score: ${computerScore}`
+            container.appendChild(displayResult);
+            container.appendChild(displayPlayerScore);
+            container.appendChild(displayComputerScore);
+
+            if (yourScore >= 5){
+                displayFinalResult.textContent = 'You won the game';
+                container.appendChild(displayFinalResult);
+            }else if (computerScore >=5){
+                displayFinalResult.textContent = 'Computer won the game';
+                container.appendChild(displayFinalResult);
+            }
+        });
+    });
 }
 game();
 
